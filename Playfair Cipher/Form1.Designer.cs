46043a468260c8acab967ebb2a9f8b48f6ce019e
malhariri92 <malhariri@student.cscc.edu>
2020-05-18 00:57:29 -0400

    Initial commit

---
+++ Playfair Cipher/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Exercise5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.secretWord = new System.Windows.Forms.TextBox();
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.translateBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cipher \"Secret\" Word:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(123, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Input Text:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(112, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Output Text:";
+            // 
+            // secretWord
+            // 
+            this.secretWord.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secretWord.Location = new System.Drawing.Point(204, 76);
+            this.secretWord.Multiline = true;
+            this.secretWord.Name = "secretWord";
+            this.secretWord.Size = new System.Drawing.Size(198, 30);
+            this.secretWord.TabIndex = 3;
+            this.secretWord.TextChanged += new System.EventHandler(this.secretWord_TextChanged);
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.Location = new System.Drawing.Point(204, 121);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(513, 72);
+            this.input.TabIndex = 4;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // output
+            // 
+            this.output.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(204, 227);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.Size = new System.Drawing.Size(513, 72);
+            this.output.TabIndex = 5;
+            // 
+            // translateBtn
+            // 
+            this.translateBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translateBtn.Location = new System.Drawing.Point(204, 366);
+            this.translateBtn.Name = "translateBtn";
+            this.translateBtn.Size = new System.Drawing.Size(95, 23);
+            this.translateBtn.TabIndex = 6;
+            this.translateBtn.Text = "Translate Text";
+            this.translateBtn.UseVisualStyleBackColor = true;
+            this.translateBtn.Click += new System.EventHandler(this.translateBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(327, 366);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 7;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeBtn.Location = new System.Drawing.Point(642, 366);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(75, 23);
+            this.closeBtn.TabIndex = 8;
+            this.closeBtn.Text = "Close";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 450);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.translateBtn);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.secretWord);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Playfair Cipher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox secretWord;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button translateBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button closeBtn;
+    }
+}
+
---
+++ Playfair Cipher/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise5
+{
+    // Mutasem Alhariri 03/20/2020
+    public partial class Form1 : Form
+    {
+        char[,] matrix = new char[5, 5];
+        string alpha = "ABCDEFGHIKLMNOPQRSTUVWXYZ"; // Contains all alphabet characters except the letter 'J'.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            secretWord.Text = "";
+            input.Text = "";
+            output.Text = "";
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void secretWord_TextChanged(object sender, EventArgs e)
+        {
+            secretWord.BackColor = Color.White;
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+            input.BackColor = Color.White;
+        }
+        private void translateBtn_Click(object sender, EventArgs e)
+        {
+            if (IsNotEmpty())
+            {
+                if (IsValidWord())
+                {
+
+                    FillMatrix(PrepareChars(secretWord.Text.ToUpper()));
+                    output.Text = Translate(input.Text.ToUpper());
+
+                }
+
+            }
+        }
+        // Checks if any of the required texts is empty
+
+        private bool IsNotEmpty()
+        {
+            if (secretWord.Text == "")
+            {
+                secretWord.BackColor = Color.Red;
+                MessageBox.Show("A secret word is required.", "Input Left Empty");
+                return false;
+            }
+            else if (input.Text == "")
+            {
+                input.BackColor = Color.Red;
+                MessageBox.Show("Please enter text you wish to translate.", "Input Left Empty");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        // Checks if the secret word is valid
+        private bool IsValidWord()
+        {
+            Regex re = new Regex(@"^[a-zA-Z]+$");
+
+            if (re.IsMatch(secretWord.Text))
+            {
+                return true;
+            }
+            else
+            {
+                secretWord.BackColor = Color.Red;
+                MessageBox.Show("Secret Words cannot contain spaces, numbers or punctuations.", "Invalid Word");
+                return false;
+            }
+        }
+        // Takes a string that contains chars from the secret word and the remaining chars in the alphapet
+        // and fills the matrix.
+        private void FillMatrix(string matrixString)
+        {
+            int x = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = matrixString[x];
+                    x++;
+                }
+            }
+
+        }
+        // Prepares a string that contains secret word chars and the remaining chars in alpha
+        // Returns a string with no duplicates
+        private string PrepareChars(string word)
+        {
+            word = word.Replace('J', 'I');
+            string allChars = "";
+
+            foreach (char c in word)
+            {
+                if (!allChars.Contains(c))
+                {
+                    allChars += c;
+                }
+            }
+            foreach (char c in alpha)
+            {
+                if (!allChars.Contains(c))
+                {
+                    allChars += c;
+                }
+            }
+            return allChars;
+        }
+        // Takes the string we want to encrypt 
+        // Returns the encrypted string
+        private string Translate(string phrase)
+        {
+            string result = "";
+
+            foreach (char c in phrase)
+            {
+                if (Char.IsLetter(c))
+                {
+                    result += EncodeLetter(c);
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+            return result;
+        }
+        // Takes a character to encode
+        // Returns the encoded char.
+        private char EncodeLetter(char c)
+        {
+            if (c.Equals('J')) c = 'I';
+
+            char letter = '\0'; // the encoded char to return
+            bool found = false; 
+
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    if (matrix[x, y].Equals(c))
+                    {
+                        letter = matrix[y, x];
+                        found = true;
+                        break;
+                    }
+                }
+                if (found) break;
+            }
+            return letter;
+        }
+    }
+}
